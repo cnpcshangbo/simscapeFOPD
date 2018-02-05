5efcfb62289777c5216d8a8d428e539e0242a8ff
Bo Shang <cnpcshangbo@gmail.com>
2018-02-05 15:47:15 +0800

    Corrected the signal name of VerticalRate_Cmd.

--- QuadRotor/work/run_simulink_plot.m
+++ QuadRotor/work/run_simulink_plot.m
@@ -1,23 +1,23 @@
 %%
-assignin('base','wb',1e-4);
-assignin('base','wh',1e-2);
+assignin('base','wb',1e-10);
+assignin('base','wh',1e-10);
 sim('../Models/Quadrotor_Controller.slx');
 figure(1);
 hold on
 plot(Altitude_Meas.Time,Altitude_Meas.Data,'--');
-itae1=num2str(itae.Data(end));
+itae1=[num2str(itae.Data(end),10) ',wb=' num2str(wb) ',wh=' num2str(wh)];
 %legend('wh=2e-3');
 grid on
 %%
-assignin('base','wb',1e-4);
-assignin('base','wh',1e-1);
+assignin('base','wb',1e-10);
+assignin('base','wh',1e-9);
 sim('../Models/Quadrotor_Controller.slx');
 figure(1);
 hold on
 plot(Altitude_Meas.Time,Altitude_Meas.Data,'-');
-itae2=num2str(itae.Data(end));
+itae2=[num2str(itae.Data(end),10) ',wb=' num2str(wb) ',wh=' num2str(wh)];
 
-legend(['ITAE=' num2str(itae1)],['ITAE=' num2str(itae2)]);
+legend(['ITAE=' itae1],['ITAE=' itae2]);
 %legend('wh=3e-3');
 
 % grid on
