9424d54ce545ae45ef0b0be6cb38b3e0e2a53f05
Bo Shang <cnpcshangbo@gmail.com>
2018-03-01 15:52:09 +0800

    I need to recheck z domain pole.

---
+++ QuadRotor/work/zz.m
@@ -0,0 +1,13 @@
+gam=0.9693;
+ww=[1e-4,1e4];n=6;
+wb=ww(1); wh=ww(2); 
+kF=0;Ts=0.01;
+if kF==1, G=ousta_fod(gam,n,wb,wh);
+else, G=new_fod(gam,n,wb,wh); end
+G=c2d(G,Ts,'tustin');
+num=G.num{1}; den=G.den{1}; T=1/wh; str='Fractional\n';
+if isnumeric(gam)
+	if gam>0, str=[str, 'Der  s^' num2str(gam) ];
+	else, str=[str, 'Int  s^{' num2str(gam) '}']; end
+else, str=[str, 'Der  s^gam']; end
+if kF1==1, G=G*tf(1,[T 1]); end
