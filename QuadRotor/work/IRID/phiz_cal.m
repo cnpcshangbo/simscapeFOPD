26feba6314ff28d1164afdeb443ab21a9c1df1e2
Bo Shang <cnpcshangbo@gmail.com>
2018-03-11 00:13:48 +0800

    Updated param

--- QuadRotor/work/IRID/phiz_cal.m
+++ QuadRotor/work/IRID/phiz_cal.m
@@ -1,11 +1,13 @@
 gam=0.9693;
 Ts=.01;
 dfod=irid_fod(gam,Ts,5);
-
-cd=7.9733*(1+0.2238*dfod);
+kp=9.9498;
+ki=0.1631;
+cd=kp*(1+ki*dfod);
 
 s=tf('s');
-p=0.99217/(0.2486*s+1)/s;
+K=1.102, T1=0.17936
+p=K/(T1*s+1)/s;
 pd=c2d(p,Ts,'tustin');%plant
 
 phiz=cd*pd/(1+cd*pd);
